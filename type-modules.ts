d20a853cb129ea107eb38c237c37f82e47ab8239
Verc James <vercjames@gmail.com>
2023-03-29 23:02:37 -0500

    template update

--- type-modules.ts
+++ type-modules.ts
@@ -5,6 +5,7 @@ declare module "*.css";
 declare module "*.sass";
 declare module "*.scss";
 declare module "*.html";
+declare module "*.json";
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
